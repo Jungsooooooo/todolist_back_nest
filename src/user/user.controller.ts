829f187e06912bd8c7a92727835eb68c237343a4
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-05 21:46:22 +0900

    string 타입 get 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService } from 'src/app.service';
+import { UUID } from 'crypto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/st')
+  getString(): string {
+    return 'please do some';
+  }
+}
