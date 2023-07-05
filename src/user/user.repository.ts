b11565534c51229ff02f2f811ce72ed99710cbbf
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-05 23:58:32 +0900

    UUID를 통한 데이터 get 구현 진행중

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,14 +2,14 @@ import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { Injectable } from '@nestjs/common';
 
+import { UUID } from 'crypto';
+
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private readonly dataSource: DataSource) {
-    const baseRepository = dataSource.getRepository(User);
-    super(
-      baseRepository.target,
-      baseRepository.manager,
-      baseRepository.queryRunner,
-    );
+    super(User, dataSource.createEntityManager());
+  }
+  async getByUUID(id: UUID) {
+    return this.findOne({ where: { id } });
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,24 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 import { UUID } from 'crypto';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
+  constructor(private usersRepository: UserRepository) {}
 
-  findAll(): Promise<User[]> {
-    return this.usersRepository.find();
-  }
-
-  findOne(uid: UUID): Promise<User | null> {
-    return this.usersRepository.findOneBy({ uid });
-  }
-
-  async remove(uid: UUID): Promise<void> {
-    await this.usersRepository.delete(uid);
+  findAll(id: UUID): Promise<User> {
+    return this.usersRepository.getByUUID(id);
   }
 }
