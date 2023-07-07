5ff2d49918382318469cf7b3bb13fd543a0f60ef
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-07 22:58:05 +0900

    repository 사용을 위한 설정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
-import { TypeOrmExModule } from 'src/typeorm-ex.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, UserRepository],
+  exports: [UserService, UserRepository],
   controllers: [UserController],
 })
 export class UserModule {}
