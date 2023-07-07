826505db30a196a94b9d783aacb17bd286ead254
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-07 22:57:43 +0900

    repository 모든 유저 호출 하기 완료

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,14 +1,16 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { Injectable } from '@nestjs/common';
+// import { CustomRepository } from 'src/typeorm-ex.decorator';
 
 import { UUID } from 'crypto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
-  constructor(private readonly dataSource: DataSource) {
+  constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
+
   async getByUUID(id: UUID) {
     return this.findOne({ where: { id } });
   }
