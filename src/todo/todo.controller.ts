e0f5295141b8b01f21ab585b3015f9b7279a63b5
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-13 22:48:11 +0900

    todo CRUD 기능 구현 완료

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.entity';
+
+import { UUID } from 'crypto';
+import { TodoRequestDto } from './todo.requestdto';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+
+  @Get('all')
+  getTodoAll(): Promise<Todo[]> {
+    return this.todoService.getAll();
+  }
+
+  @Get('/:id')
+  getTodo(@Param('id') uid: UUID): Promise<Todo> {
+    return this.todoService.getTodo(uid);
+  }
+
+  @Post()
+  createTodo(@Body() todoRequestDto: TodoRequestDto) {
+    return this.todoService.createTodo(todoRequestDto);
+  }
+
+  @Delete('/:id')
+  deleteTodo(@Param('id') uid: UUID): Promise<void> {
+    return this.todoService.deleteTodo(uid);
+  }
+
+  @Put('/:id')
+  updateTodo(
+    @Param('id') uid: UUID,
+    @Body() todoRequestDto: TodoRequestDto,
+  ): Promise<Todo> {
+    return this.todoService.updateTodo(uid, todoRequestDto);
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { TodoRepository } from './todo.repository';
+import { Todo } from './todo.entity';
+import { UUID } from 'crypto';
+import { TodoRequestDto } from './todo.requestdto';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private todoRepository: TodoRepository) {}
+
+  getAll(): Promise<Todo[]> {
+    return this.todoRepository.find();
+  }
+
+  getTodo(uid: UUID): Promise<Todo> {
+    return this.todoRepository.getByUUID(uid);
+  }
+
+  async createTodo(todoRequestDto: TodoRequestDto) {
+    const { do: string, endDate } = todoRequestDto;
+
+    const todo = this.todoRepository.create({
+      do: string,
+      endDate,
+    });
+
+    await this.todoRepository.save(todo);
+
+    return todo;
+  }
+
+  async deleteTodo(uid: UUID): Promise<void> {
+    this.todoRepository.delete(uid);
+  }
+
+  async updateTodo(uid: UUID, todoRequestDto): Promise<Todo> {
+    await this.todoRepository.update(uid, todoRequestDto);
+    return this.todoRepository.getByUUID(uid);
+  }
+}
