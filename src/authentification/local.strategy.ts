81246ecd4eb0d60a438c577b9d6f8de3255fe0d8
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-09-09 23:38:52 +0900

    local strategy 추가

---
+++ src/authentification/local.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import AuthService from './authentification.service';
+import { UserService } from 'src/user/user.service';
+import { ExtractJwt } from 'passport-jwt';
+import { User } from 'src/user/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from 'src/user/user.repository';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      secretOrkey: 'Secret1234',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  async validate(payload: { userId: string }) {
+    const { userId } = payload;
+    const user: User = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (user === null) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
