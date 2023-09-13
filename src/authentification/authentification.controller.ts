16b3fd3f44a9c6991386e72066802e120d273cce
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-09-13 15:37:32 +0900

    public 추가 및 todo 권한 설정

--- src/authentification/authentification.controller.ts
+++ src/authentification/authentification.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Get, UseGuards } from '@nestjs/common';
 import AuthService from './authentification.service';
-import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
 import { UserRequestDto } from 'src/user/user.requestdto';
 import { Post } from '@nestjs/common';
 import { JwtAuthGuard } from './auth.guard';
+import { Public } from './public.decorator';
 
 @Controller('/api/authentification')
 export class AuthentificationController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
+  @Public()
   login(@Body() userRequestdto: UserRequestDto) {
     return this.authService.validateUser(userRequestdto);
   }
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { Todo } from './todo.entity';
@@ -13,22 +14,24 @@ import { Todo } from './todo.entity';
 import { UUID } from 'crypto';
 import { TodoRequestDto } from './todo.requestdto';
 import { TodoResponseDto } from './todo.responsedto';
-
-@Controller('/api/todo')
+import { JwtAuthGuard } from 'src/authentification/auth.guard';
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
   @Get('/all')
+  @UseGuards(JwtAuthGuard)
   getTodoAll(): Promise<TodoResponseDto[]> {
     return this.todoService.getTodoResponse();
   }
 
   @Get('/:id')
+  @UseGuards(JwtAuthGuard)
   getTodo(@Param('id') uid: UUID): Promise<Todo> {
     return this.todoService.getTodo(uid);
   }
 
   @Get('/:year/:month')
+  @UseGuards(JwtAuthGuard)
   getDataByYearAndMonth(
     @Param('year') year: number,
     @Param('month') month: number,
@@ -37,6 +40,7 @@ export class TodoController {
   }
 
   @Get('/date/:year/:month/:date')
+  @UseGuards(JwtAuthGuard)
   getDataByDate(
     @Param('year') year: number,
     @Param('month') month: number,
@@ -46,16 +50,19 @@ export class TodoController {
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   createTodo(@Body() todoRequestDto: TodoRequestDto) {
     return this.todoService.createTodo(todoRequestDto);
   }
 
   @Delete('/:id')
+  @UseGuards(JwtAuthGuard)
   deleteTodo(@Param('id') uid: UUID): Promise<void> {
     return this.todoService.deleteTodo(uid);
   }
 
   @Put('/:id')
+  @UseGuards(JwtAuthGuard)
   updateTodo(
     @Param('id') uid: UUID,
     @Body() todoRequestDto: TodoRequestDto,
